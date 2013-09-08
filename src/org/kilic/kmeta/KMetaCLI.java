149aeaed708556480e6b04c2358040c53fbf1eb9
aykutkilic <aykutkilic80@gmail.com>
2013-09-08 23:53:13 +0200

    added sample file

--- src/org/kilic/kmeta/KMetaCLI.java
+++ src/org/kilic/kmeta/KMetaCLI.java
@@ -1,7 +1,12 @@
 package org.kilic.kmeta;
 
+import org.antlr.v4.runtime.ANTLRFileStream;
+
+import java.io.IOException;
+
 public class KMetaCLI {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        KMetaLexer lexer = new KMetaLexer(new ANTLRFileStream(args[0]));
+
     }
 }
