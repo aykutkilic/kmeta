61ecd810ea99be0d1b92307c4aae945d3c12dd48
aykutkilic <aykutkilic80@gmail.com>
2013-09-13 00:32:00 +0200

    now producing a preliminary grammar.

--- src/main/java/org/kilic/kmeta/Concept.java
+++ src/main/java/org/kilic/kmeta/Concept.java
@@ -1,13 +1,15 @@
 package org.kilic.kmeta;
 
-import java.util.HashSet;
-import java.util.Set;
+import com.google.common.base.Joiner;
+
+import java.util.*;
 
 public class Concept extends BaseScope {
     private ExecutionUnit executionUnit;
     private boolean isAbstract;
     private Set<Concept> parents;
-    private Set<Concept> children;
+    private Map<String, Concept> children;
+    private Map<String, Object>  properties;
     private String syntax;
 
     public Concept(String name, ExecutionUnit container) {
@@ -15,15 +17,20 @@ public class Concept extends BaseScope {
 
         executionUnit = container;
         parents = new HashSet<>();
-        children = new HashSet<>();
+        children = new HashMap<>();
+        properties = new HashMap<>();
     }
 
     public String getSyntax() {
         return syntax;
     }
 
-    public Set<Concept> getChildren() {
+    public Map<String, Concept> getChildren() {
         return children;
+    }
+
+    public Map<String, Object> getProperties() {
+        return properties;
     }
 
     public Set<Concept> getParents() {
@@ -44,5 +51,18 @@ public class Concept extends BaseScope {
 
     public ExecutionUnit getExecutionUnit() {
         return executionUnit;
+    }
+
+    public String getCurrentGrammar() {
+        StringBuilder result = new StringBuilder();
+
+        result.append(getShortName()+":\n");
+        result.append("  " + Joiner.on(" | ").join(children.values().stream().map( Concept::getShortName).iterator()) + "\n");
+        if( syntax != null && !getSyntax().isEmpty())
+            result.append("  " + getSyntax() + "\n");
+
+        result.append(";\n\n");
+
+        return result.toString();
     }
 }
--- src/main/java/org/kilic/kmeta/MetaProcessor.java
+++ src/main/java/org/kilic/kmeta/MetaProcessor.java
@@ -1,11 +1,14 @@
 package org.kilic.kmeta;
 
+import com.google.common.base.Joiner;
 import com.google.inject.Inject;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MetaProcessor {
     private ExecutionUnit executionUnit;
@@ -26,7 +29,32 @@ public class MetaProcessor {
         parser.setBuildParseTree(true);
         conceptsLinker.reset();
         processExecutionUnit(parser.executionUnit());
-        conceptsLinker.resolve();
+        Collection<String> unresolvedItems = conceptsLinker.resolve();
+
+        String currentGrammar = computeCurrentGrammar();
+    }
+
+    public String computeCurrentGrammar() {
+        StringBuilder result = new StringBuilder();
+
+        concepts.values().forEach(c-> result.append(c.getCurrentGrammar()));
+
+        // dumping the footer.
+        result.append("INT        : NUM+;\n");
+        result.append("REAL       : NUM* '.' NUM*;\n");
+        result.append("ID         : ALPHA (ALNUM | '_')*;");
+        result.append("STRING     : '\"'  (  ~('\"'|'\\\\'|'\\n'|'\\r') )*  '\"'\n");
+        result.append("{\n");
+        result.append("    setText( org.antlr.v4.misc.CharSupport.getStringFromGrammarStringLiteral( getText() ) );\n");
+        result.append("};\n");
+        result.append("\n");
+        result.append("fragment ALNUM      : ALPHA | [0-9];\n");
+        result.append("fragment ALPHA      : [a-zA-Z];\n");
+        result.append("fragment NUM        : [0-9];\n");
+        result.append("fragment NEWLINE    : [\\r\\n]+;\n");
+        result.append("fragment HEX_DIGIT  : [0-9a-fA-F];\n");
+
+        return result.toString();
     }
 
     private void processExecutionUnit(KMetaParser.ExecutionUnitContext ctx) {
@@ -49,11 +77,16 @@ public class MetaProcessor {
                 conceptsLinker.registerListener(id.getText(), item -> newConcept.getParents().add(item));
             });
 
-        ctx.metaExpression().stream().findFirst().ifPresent(e -> newConcept.setSyntax(e.getText()));
+        ctx.metaExpression().stream().findFirst().ifPresent(e -> newConcept.setSyntax(getSyntax(e)));
 
         concepts.put(newConcept.getFQN(), newConcept);
+        conceptsLinker.linkFQNToObject(newConcept.getShortName(), newConcept);
     }
 
     private void processDefinition(Concept newConcept, KMetaParser.DefinitionContext c) {
+    }
+
+    private String getSyntax(KMetaParser.MetaExpressionContext ctx) {
+        return Joiner.on("").join(ctx.children.subList(2,ctx.children.size()-1));
     }
 }
